1f802d22535aff3bf6042d30265b7893b4309abf
matralves <malves@sauter.digital>
2024-07-15 18:31:09 -0300

    cicd

--- src/main/java/com/pratopronto/PratoProntoApplication.java
+++ src/main/java/com/pratopronto/PratoProntoApplication.java
@@ -3,10 +3,8 @@ package com.pratopronto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {ContextStackAutoConfiguration.class})
-@EnableJpaRepositories()
 public class PratoProntoApplication {
 
     public static void main(String[] args) {
